a3ec20a2e647d602e375ecb31cfde4ce7cf90779
UserPC <UserPC@DESKTOP-5JSVERC>
2023-02-08 17:31:07 +0300

    Добавьте файлы проекта.

---
+++ 2/Program.cs
@@ -0,0 +1,32 @@
+﻿
+using System.Diagnostics.Contracts;
+using System.Runtime.CompilerServices;
+Person Ivan = new Person();
+Person Kirill = new Person("Кирилл", 18);
+Ivan.move("Ivan");
+Kirill.talk("Kirill");
+public class Person
+{
+    string fullName = "None";
+    int age = 18;
+
+    public void move(string Person)
+    {
+        Console.WriteLine("Такой-то " + Person + " говорит");
+    }
+    public void talk(string Person)
+    {
+        Console.WriteLine("Такой-то " + Person + " говорит");
+    }
+
+    public Person() 
+    { 
+    
+    }
+
+    public Person (string fullName, int age)
+    {
+        this.fullName= fullName;
+        this.age= age;
+    }
+}
